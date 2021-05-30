b950bda78497664254d398a1472697ff598ff2ba
Eung Jin Lee <>
2021-05-30 09:01:08 +0000

    div를 GoodComponent로 추출

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div>조던 - 신발 - 신발장 - 300000원</div>
+    <GoodComponent />
   );
 }
 
 export default App;
 
+function GoodComponent() {
+  return (
+    <div>조던 - 신발 - 신발장 - 300000원</div>
+  )
+}
