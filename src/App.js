8f9323bac8ba5e89fc10f0cc96110aef0cafc672
Eung Jin Lee <>
2021-05-30 09:09:05 +0000

    함수를 표현형으로 바꾸기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div>
       <GoodComponent name="조던" category="신발" position="신발장" price={300000}/>
-      <GoodComponent2 name="티셔츠" category="옷" position="박스1" price={30000}/>
+      <GoodComponent name="티셔츠" category="옷" position="박스1" price={30000}/>
     </div>
   );
 }
 
 export default App;
 
-function GoodComponent({name, category, position, price}) {
-  return (
-    <div>{`${name} - ${category} - ${position} - ${price}원`}</div>
-  )
-}
-
-const GoodComponent2 = ({name, category, position, price}) => {
+const GoodComponent = ({name, category, position, price}) => {
   return (
     <div>{`${name} - ${category} - ${position} - ${price}원`}</div>
   )
