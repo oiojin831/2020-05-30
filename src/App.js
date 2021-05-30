3cd8d5ad8ec73c88e87383a01004a3a0895a2f02
Eung Jin Lee <>
2021-05-30 10:00:39 +0000

    callback 함수 map 안에 넣기

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React from 'react';
 
 const App = () => {
-  const data = [
-    {
-      name: '맥스 97',
-      category: '신발',
-      position: "신발장",
-      price: 180000
-    },
-    {
-      name: '맥스 95',
-      category: '신발',
-      position: "box1",
-
-      price: 200000
-    },
-    {
-      name: '검정 티셔츠',
-      category: '옷',
-      position: "box1",
-      price: 30000
-    },
-    {
-      name: '자전거',
-      category: '운동기기',
-      position: "베란다",
-      price: 200000
-    }
-  ];
-  
-  const callBack = (item, index) => {
-      return  <GoodComponent
-                key={`${index}-${item.name}`} 
-                name={item.name} category={item.category} 
-                position={item.position} 
-                price={item.price}
-              />
-  }
-
   return (
     <div>
-      {data.map(callBack)}
+      {data.map((item, index) => {
+        return <GoodComponent
+          key={`${index}-${item.name}`}
+          name={item.name} category={item.category}
+          position={item.position}
+          price={item.price}
+        />
+      })}
     </div>
   );
 }
@@ -52,3 +22,31 @@ const GoodComponent = ({ name, category, position, price }) => {
     <div>{`${name} - ${category} - ${position} - ${price}원`}</div>
   )
 }
+
+const data = [
+  {
+    name: '맥스 97',
+    category: '신발',
+    position: "신발장",
+    price: 180000
+  },
+  {
+    name: '맥스 95',
+    category: '신발',
+    position: "box1",
+
+    price: 200000
+  },
+  {
+    name: '검정 티셔츠',
+    category: '옷',
+    position: "box1",
+    price: 30000
+  },
+  {
+    name: '자전거',
+    category: '운동기기',
+    position: "베란다",
+    price: 200000
+  }
+];
